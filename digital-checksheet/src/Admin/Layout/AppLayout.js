96eea52da8aa03b4462c5f7e559b3a09a177995a
Kadeejath Salaha <codestudy475@gmail.com>
2024-06-06 12:20:01 +0530

    move header to layout folder

--- digital-checksheet/src/Admin/Layout/AppLayout.js
+++ digital-checksheet/src/Admin/Layout/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import Header from "../Checklist/Header";
 import "./AppLayout.css";
+import Header from "./Header";
 
 const AppLayout = ({ children }) => {
   const location = useLocation();
