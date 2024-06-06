847845bfac2f6a62fcf5622f1ad3b6b376b6b52e
Kadeejath Salaha <codestudy475@gmail.com>
2024-06-06 11:16:24 +0530

    created diff layout folder and all set

---
+++ digital-checksheet/src/Admin/Layout/AppLayout.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Header from "../Checklist/Header";
+import "./AppLayout.css";
+
+const AppLayout = ({ children }) => {
+  const location = useLocation();
+  const hideSidebarPaths = ["/", "/login", "/register"];
+  const showSidebar = !hideSidebarPaths.includes(location.pathname);
+
+  return (
+    <div className="container">
+      <Header />
+      <div className="main-layout">
+        {showSidebar && (
+          <aside className="sidebar">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/master">Master</Link>
+                </li>
+                <li>
+                  <Link to="/checklist">Checklist</Link>
+                </li>
+                <li>
+                  <Link to="/video">View</Link>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+        )}
+        <main className="content">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
--- digital-checksheet/src/App.js
+++ digital-checksheet/src/App.js
@@ -1,57 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Checklist from "./Admin/Checklist/Checklist";
-import Header from "./Admin/Checklist/Header";
 
-import "./App.css";
 import Login from "./user/Login/Login";
-import Register from "./user/Register/Register";
+// import Register from "./user/Register/Register";
 import { AppProvider } from "./Admin/Master/Context";
 import Master from "./Admin/Master/Master";
+import AppLayout from "./Admin/Layout/AppLayout";
 
 function App() {
   return (
     <AppProvider>
       <Router>
-        <div className="container">
-          <Header />
-          <div className="main-layout">
-            <aside className="sidebar">
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link to="/master">Master</Link>
-                  </li>
-                  <li>
-                    <Link to="/checklist">Checklist</Link>
-                  </li>
-                  <li>
-                    <Link to="/video">View</Link>
-                  </li>
-                </ul>
-              </nav>
-            </aside>
-            <main className="content">
-              <Routes>
-                <Route
-                  path="/"
-                  element={<h1>Welcome to the Digital Checksheet</h1>}
-                />
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/login" element={<Login />} />
-
-                <Route path="/dashboard" element={<h1>Dashboard Content</h1>} />
-                <Route path="/master" element={<Master />} />
-                <Route path="/checklist" element={<Checklist />} />
-                <Route path="/video" element={<h1>View</h1>} />
-                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-              </Routes>
-            </main>
-          </div>
-        </div>
+        <AppLayout>
+          <Routes>
+            {/* <Route path="/register" element={<Register />}></Route> */}
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<h1>Dashboard Content</h1>} />
+            <Route path="/master" element={<Master />} />
+            <Route path="/checklist" element={<Checklist />} />
+            <Route path="/video" element={<h1>View</h1>} />
+            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+          </Routes>
+        </AppLayout>
       </Router>
     </AppProvider>
   );
